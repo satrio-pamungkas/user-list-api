6e6bcf18978dae542f4a428a773059ca96c0ba45
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-05-17 22:14:41 +0700

    feat: added swagger docs

--- index.ts
+++ index.ts
@@ -1,19 +1,23 @@
-import express, { Express, Response } from 'express';
+import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import client from './src/configs/redis.config';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 import { UserRoute } from './src/routes/user.route';
 
 dotenv.config();
 
 const app: Express = express();
 const PORT: any = process.env.PORT;
+const swaggerDoc: any = YAML.load('./docs/swagger.yaml');
 
 app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 
 app.use('/api/v1/users/', UserRoute());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(PORT, async () => {
     await client.connect()
