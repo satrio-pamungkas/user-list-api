999d960133209669b7625e8c48044615a4cd176b
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-05-23 22:07:21 +0700

    feat: added spec util test

--- src/utils/helper.util.ts
+++ src/utils/helper.util.ts
@@ -1,6 +1,10 @@
 import { Paginate } from "../types";
 
 export const getPagination = async (page: number, size: number): Promise<Paginate> => {
+    if (page <= 0 || size <= 0) {
+        throw new Error('Page and size cannot be negative value or zero');
+    }
+
     const limit: number = size ? +size : 10;
     const offset: number = page ? page * limit : 1;
 
--- test/controllers/utils.test.ts
+++ test/controllers/utils.test.ts
@@ -1,5 +1,80 @@
 import { getPagination } from "../../src/utils/helper.util";
 
-describe("Return pagination format as limit and offser", () => {
-    test("")
+describe("Return error when pagination format as limit and offset breaking the rule", () => {
+    test("Page input with negative value and size input with positive value", async () => {
+        try {
+            const { limit, offset } = await getPagination(-1,5);
+            console.log(limit);
+            console.log(offset);
+
+        } catch (error: any) {
+            expect(error.message).toBe("Page and size cannot be negative value or zero");
+        }
+    });
+
+    test("Page input with positive value and size input with negative value", async () => {
+        try {
+            const { limit, offset } = await getPagination(2,-5);
+            console.log(limit);
+            console.log(offset);
+
+        } catch (error: any) {
+            expect(error.message).toBe("Page and size cannot be negative value or zero");
+        }
+    });
+
+    test("Page input with negative value and size input with negative value", async () => {
+        try {
+            const { limit, offset } = await getPagination(-2,-5);
+            console.log(limit);
+            console.log(offset);
+
+        } catch (error: any) {
+            expect(error.message).toBe("Page and size cannot be negative value or zero");
+        }
+    });
+
+    test("Page input with zero value and size input with negative value", async () => {
+        try {
+            const { limit, offset } = await getPagination(0,-5);
+            console.log(limit);
+            console.log(offset);
+
+        } catch (error: any) {
+            expect(error.message).toBe("Page and size cannot be negative value or zero");
+        }
+    });
+
+    test("Page input with negative value and size input with zero value", async () => {
+        try {
+            const { limit, offset } = await getPagination(-2,0);
+            console.log(limit);
+            console.log(offset);
+
+        } catch (error: any) {
+            expect(error.message).toBe("Page and size cannot be negative value or zero");
+        }
+    });
+
+    test("Page input with zero value and size input with positive value", async () => {
+        try {
+            const { limit, offset } = await getPagination(0,5);
+            console.log(limit);
+            console.log(offset);
+
+        } catch (error: any) {
+            expect(error.message).toBe("Page and size cannot be negative value or zero");
+        }
+    });
+
+    test("Page input with positive value and size input with zero value", async () => {
+        try {
+            const { limit, offset } = await getPagination(2,0);
+            console.log(limit);
+            console.log(offset);
+
+        } catch (error: any) {
+            expect(error.message).toBe("Page and size cannot be negative value or zero");
+        }
+    });
 })
